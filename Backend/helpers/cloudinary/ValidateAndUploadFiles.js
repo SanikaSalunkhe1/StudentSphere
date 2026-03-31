946bc0d17eeb853f1e57673002fe8aa00b111e23
AasthaOswal <aasthaoswal29@gmail.com>
2026-03-31 13:13:13 +0530

    Made backend same as internship repo

--- Backend/helpers/cloudinary/ValidateAndUploadFiles.js
+++ Backend/helpers/cloudinary/ValidateAndUploadFiles.js
@@ -42,7 +42,7 @@ const validateAndUploadFiles = async (filesObj, fileConfigs) => {
             const { fieldName } = config;
             const file = filesObj?.[fieldName]?.[0];
             const uploadOptions = {
-                folder: "studentWebsite",
+                folder: "StudentSphere",
                 resource_type: "auto",
                 access_mode: "public",
                 ...(config.cloudinaryOptions || {}) // merge extra options if provided
